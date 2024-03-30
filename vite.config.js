37f168c1c1124a06814975bf9d9266b2cb3b3503
khushbu kumari <khushbulike7@gmail.com>
2024-03-30 17:13:06 +0530

    file-updated

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,15 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    target: "es2022"
+  },
+  esbuild: {
+    supported: {
+      'top-level-await': true
+    },
+  },
   base: "/sample-portfolio-2/",
+
   plugins: [react()],
 })
