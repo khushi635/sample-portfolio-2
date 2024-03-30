b98d8314976d9c4afc034cf3c56f961fec3ca0af
khushbu kumari <khushbulike7@gmail.com>
2024-03-30 17:06:54 +0530

    js-file-updated

--- src/components/GetData.js
+++ src/components/GetData.js
@@ -18,8 +18,8 @@ import Projects from "./projects/Projects.jsx"
        
     // }
 
-    const response =  await fetch(url)
-    const data = await response.json()
+    let response =  await fetch(url)
+    let data = await response.json()
    
     // console.log(data)
     // console.log(data.user.skills[0].image.url)
